cfd5757483e7a661f8a0d62d606d736d1440fa12
mehedinoorkhan <mehedinoorkhan16@gmail.com>
2026-03-03 05:14:01 +0600

    middleware is done

--- proxy.ts
+++ proxy.ts
@@ -2,12 +2,16 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@/lib/auth";
 
 export async function proxy(request: NextRequest) {
-
+ const session = await getSession()
 
     const isDashboardPage = request.nextUrl.pathname.startsWith("/dashboard");
-    const session = await getSession()
-    if(!session?.user){
+    if(isDashboardPage && !session?.user){
         return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+    
+    const isSignInPage = request.nextUrl.pathname.startsWith("/sign-in");
+    if(isSignInPage && session?.user){
+        return NextResponse.redirect(new URL("/sign-up", request.url));
     }
 
     return NextResponse.next();
